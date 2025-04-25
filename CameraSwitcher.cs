ffb8a2fd4e6a85ebd6b80c582da8b49bc2450dfe
Andrey <166303827+FoxikkS@users.noreply.github.com>
2025-04-25 22:22:20 +0500

    Add files via upload

---
+++ CameraSwitcher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [Header("Security Camera Feeds")]
+    [SerializeField] private List<RenderTexture> cameraFeeds;
+
+    [Header("Camera UI Canvas")]
+    [SerializeField] private Canvas cameraCanvas;
+
+    private RawImage _cameraDisplay;
+    private int _currentCameraIndex;
+
+    private void Start()
+    {
+        _cameraDisplay = cameraCanvas.GetComponent<RawImage>();
+        _cameraDisplay.texture = cameraFeeds[_currentCameraIndex];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _currentCameraIndex--;
+            _currentCameraIndex = Mathf.Clamp(_currentCameraIndex, 0, cameraFeeds.Count - 1);
+            UpdateCamera();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _currentCameraIndex++;
+            _currentCameraIndex = Mathf.Clamp(_currentCameraIndex, 0, cameraFeeds.Count - 1);
+            UpdateCamera();
+        }
+    }
+
+    private void UpdateCamera()
+    {
+        _cameraDisplay.texture = cameraFeeds[_currentCameraIndex];
+    }
+}
